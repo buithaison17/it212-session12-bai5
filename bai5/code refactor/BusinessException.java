c782b73260bb99681fc5bdbd51c763db96aad0d5
buithaison17 <125809724+buithaison17@users.noreply.github.com>
2026-07-02 15:34:38 +0700

    Upload bai5/code refactor/BusinessException.java

---
+++ bai5/code refactor/BusinessException.java
@@ -0,0 +1,9 @@
+package com.example.ekyc.exception;
+
+public class BusinessException extends RuntimeException {
+
+    public BusinessException(String message) {
+        super(message);
+    }
+
+}
